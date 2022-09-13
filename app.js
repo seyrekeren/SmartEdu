71230ad52891a159c2e4a89c4d5cb7046b1af248
eren.seyrek <seyrekeren@hotmail.com>
2022-09-13 16:01:35 +0300

    first lesson

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+
+const app = express();
+
+//Template Engine
+app.set("view engine", "ejs");
+
+
+//Middlewares
+app.use(express.static("public"));
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`App started on port ${port}`);
+});
+
+
+app.get('/', (req, res) => {
+    res.status(200).render('index', {
+        page_name: "index"
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about',
+    });
+});
+
