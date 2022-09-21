8c34421037870742a6e574a9eba741ac901738e7
eren.seyrek <seyrekeren@hotmail.com>
2022-09-21 11:26:56 +0300

    lesson 13

--- controllers/authController.js
+++ controllers/authController.js
@@ -28,7 +28,7 @@ exports.loginUser = async(req, res) => {
           if (same) {
             req.session.userID = user._id;
             // USER SESSION
-            res.status(200).redirect('/');
+            res.status(200).redirect('/users/dashboard');
           }
         });
       }
@@ -46,3 +46,11 @@ exports.logoutUser = (req, res) => { //destroy çıkış methodu
     res.redirect('/');
   })
 }
+
+exports.getDashboardPage = async (req, res) => {
+  const user = await User.findOne({_id:req.session.userID})
+  res.status(200).render('dashboard', {
+    page_name: 'dashboard',
+    user
+  });
+}; 
