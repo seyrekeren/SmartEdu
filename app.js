e80f6399f250a0700c388ec82f4cf4ebc6ce6ee7
eren.seyrek <seyrekeren@hotmail.com>
2022-10-18 13:57:08 +0300

    finall

--- app.js
+++ app.js
@@ -1,66 +1,69 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('express');
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
-const pageRoute = require('../SmarEduNew/routes/pageRoute');
-const courseRoute = require('../SmarEduNew/routes/courseRoute');
-const categoryRoute = require('../SmarEduNew/routes/categoryRoute');
-const userRoute = require('../SmarEduNew/routes/userRoute');
+const flash = require('connect-flash');
+const methodOverride = require('method-override');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
+const categoryRoute = require('./routes/categoryRoute');
+const userRoute = require('./routes/userRoute');
 
 const app = express();
 
-
-
-//connect db
-mongoose.connect('mongodb://localhost/smartEdu-db',{
+//Connect DB
+mongoose
+  .connect('mongodb://localhost/smartedu-db', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
     useCreateIndex: true,
-}).then(() => {
-    console.log('DB connected');
-});
-
-//Template Engine
-app.set("view engine", "ejs");
-
-
-//Global Variables her yerden erişilibilen
-global.userIN = null; //false anlamına da gelir js 
-
-
-//Middlewares
-app.use(express.static("public"));
-app.use(bodyParser.json()) // for parsing application/json
-app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-app.use(
-    session({
-      secret: 'my_keyboard_cat', // Buradaki texti değiştireceğiz.
-      resave: false,
-      saveUninitialized: true,
-      store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartEdu-db' })
-    })
-  );
-
-
-
-app.use('/', pageRoute);
-app.use('/courses',courseRoute);
-app.use('/categories',categoryRoute);
-app.use('/users',userRoute);
-app.use('*', (req, res, next) => { //* hani url gelsede yap demek
-    userIN = req.session.userID;
-    next();
+  })
+  .then(() => {
+    console.log('DB Connected Successfully');
   });
 
+//Template Engine
+app.set('view engine', 'ejs');
+
+//Global Variable
+
+global.userIN = null;
+
+//Middlewares
+app.use(express.static('public'));
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(
+  session({
+    secret: 'my_keyboard_cat',
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({ mongoUrl: 'mongodb://localhost/smartedu-db' }),
+  })
+);
+app.use(flash());
+app.use((req, res, next) => {
+  res.locals.flashMessages = req.flash();
+  next();
+});
+app.use(
+  methodOverride('_method', {
+    methods: ['POST', 'GET'],
+  })
+);
+
+//Routes
+app.use('*', (req, res, next) => {
+  userIN = req.session.userID;
+  next();
+});
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+app.use('/categories', categoryRoute);
+app.use('/users', userRoute);
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App started on port ${port}`);
+  console.log(`App started on port ${port}`);
 });
-
-
-
-
-
